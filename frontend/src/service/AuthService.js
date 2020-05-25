520ac70d8b679c6c8c2197504b98ec181ec0f1c0
Petr Váňa <43939385+st52572@users.noreply.github.com>
2020-05-25 15:08:35 +0200

    Update AuthService.js

--- frontend/src/service/AuthService.js
+++ frontend/src/service/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = 'http://localhost:8080/token/';
+const USER_API_BASE_URL = 'https://quizer-st52572.herokuapp.com/token/';
 
 class AuthService {
 
