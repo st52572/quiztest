fe989e6b18102ba8789b4bfc05b02f7e3bd1c180
Petr Váňa <43939385+st52572@users.noreply.github.com>
2020-05-25 15:09:03 +0200

    Update UserService.js

--- frontend/src/service/UserService.js
+++ frontend/src/service/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AuthService from './AuthService';
 
-const USER_API_BASE_URL = 'http://localhost:8080/users';
+const USER_API_BASE_URL = 'https://quizer-st52572.herokuapp.com/';
 
 class UserService {
 
