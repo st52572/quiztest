3c36876c3ceed669b1d624f0e45838d74c26871e
Petr Váňa <43939385+st52572@users.noreply.github.com>
2020-05-29 11:15:00 +0200

    Update UserController.java

--- src/main/java/st52572/nnpia/quizer/controller/UserController.java
+++ src/main/java/st52572/nnpia/quizer/controller/UserController.java
@@ -9,7 +9,7 @@ import st52572.nnpia.quizer.model.User;
 import st52572.nnpia.quizer.model.UserDto;
 import st52572.nnpia.quizer.service.IUserService;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/users")
 public class UserController {
